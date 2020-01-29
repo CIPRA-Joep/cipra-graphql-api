dd978742ccd13f9067c51627e4214f8e9dbe4828
Joep Driesen <joep.driesen@buffl.be>
2020-01-29 16:28:00 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Babel");
+});
+
+app.listen(4000, () => {
+  console.log(`app is listening to port 4000`);
+});
